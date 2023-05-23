253088673d9ee65084ef53ffcc81d41da8c7bd9d
AerynQ <aeryn@xion.global>
2023-05-24 01:22:27 +0200

    update

--- script.js
+++ script.js
@@ -1,63 +1,112 @@
-document.getElementById('top-up-button').addEventListener('click', async function() {
-    const amount = parseFloat(document.getElementById('amount-input').value);
-    console.log(`Top up amount: ${amount}`);
+const popup = document.getElementById('popup');
+const iframe = document.getElementById('checkout-iframe');
+let referenceId = 1500; // Starting reference ID
 
-    const popup = document.getElementById('popup');
+document.getElementById('top-up-button').addEventListener('click', async function() {
+    const zarAmount = parseFloat(document.getElementById('amount-input').value);
+    console.log(`Top up amount in ZAR: ${zarAmount}`);
+    document.getElementById('close-button').addEventListener('click', function() {
+        popup.classList.add('hidden');
+    });
+
     popup.classList.remove('hidden');
-    const iframe = document.getElementById('checkout-iframe');
+
+    const dropdown = document.getElementById('product-dropdown');
+    const productName = dropdown.options[dropdown.selectedIndex].text;
 
     try {
-        const response = await axios.post('https://devp-api.xion.app/api/v2/iframe/create-product', [{
-            reference_id: "3",
-            product_name: "top up",
-            product_price: amount
-        }], {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMHhFMTMxNzJhODI5RjBiQTYyMDIwQ0M4MDJlOGQ2OGFkNDM5NjNGOTgzIiwiY2xpZW50X2lkIjoiM24zcG03NjluZzhlZHA5OGNwdWdxYmQ4MmIiLCJjbGllbnRfc2VjcmV0IjoiMWU0c2dydGU4bGxxbGo0bmVjbWtzb292a2wzdjRmM2J2cXY3Y3A1bWQ5OW5pN2U3MGFtMSIsImV4cCI6MTY4NzQ1ODQ5MiwiaWF0IjoxNjg0ODY2NDkyLCJpc3MiOiJYaW9uIEdsb2JhbCBTZXJ2aWNlIEFQSSJ9.0cvfNKXFmK-OxmQiAMotrAFy0-zh8gio2AaNFXThzb8'
+        // Fetch currency exchange rate
+        const exchangeRateResponse = await axios.get('https://api.exchangerate-api.com/v4/latest/USD');
+        const exchangeRateData = exchangeRateResponse.data;
+        const usdRate = exchangeRateData.rates.ZAR;
+
+        const usdAmount = zarAmount / usdRate;
+        console.log(`Top up amount in USD: ${usdAmount}`);
+
+        const response = await axios.post(
+            'https://devp-api.xion.app/api/v2/iframe/create-product',
+            [
+                {
+                    reference_id: referenceId.toString(),
+                    product_name: productName,
+                    product_price: usdAmount,
+                },
+            ],
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    Authorization:
+                        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMHhFMTMxNzJhODI5RjBiQTYyMDIwQ0M4MDJlOGQ2OGFkNDM5NjNGOTgzIiwiY2xpZW50X2lkIjoiM24zcG03NjluZzhlZHA5OGNwdWdxYmQ4MmIiLCJjbGllbnRfc2VjcmV0IjoiMWU0c2dydGU4bGxxbGo0bmVjbWtzb292a2wzdjRmM2J2cXY3Y3A1bWQ5OW5pN2U3MGFtMSIsImV4cCI6MTY4NzQ1ODQ5MiwiaWF0IjoxNjg0ODY2NDkyLCJpc3MiOiJYaW9uIEdsb2JhbCBTZXJ2aWNlIEFQSSJ9.0cvfNKXFmK-OxmQiAMotrAFy0-zh8gio2AaNFXThzb8',
+                },
             }
-        });
+        );
 
         const data = response.data;
         console.log('Product creation response:', data);
 
         const productToken = data[0].product_code;
         console.log(`Product token: ${productToken}`);
-        iframe.src = `https://devp-checkout.xionpay.app/?token=${productToken}`;
+        iframe.src = `https://devp-checkout.xion.app/?token=${productToken}`;
+        console.log(`Checkout URL: ${iframe.src}`);
 
-        window.addEventListener("message", async function(event) {
-            if (event.data && event.data.orderCode) {
-                popup.classList.add('hidden');
-                const message = document.getElementById('message');
-                message.textContent = 'Purchase Successful';
-                console.log('Purchase successful');
+        referenceId++; // Increment reference ID
 
-                let orderCode = event.data.orderCode;
-                let orderStatus = 'pending';
+        window.addEventListener(
+            'message',
+            async function(event) {
+                if (event.data && event.data.orderCode) {
+                    popup.classList.add('hidden');
+                    const message = document.getElementById('message');
+                    message.textContent = 'Purchase Successful';
+                    console.log('Purchase successful');
 
-                while (orderStatus !== 'successful') {
-                    const statusResponse = await axios.get(`https://devp-api.xion.app/api/v2/order/status/${orderCode}`, {
-                        headers: {
-                            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMHhFMTMxNzJhODI5RjBiQTYyMDIwQ0M4MDJlOGQ2OGFkNDM5NjNGOTgzIiwiY2xpZW50X2lkIjoiM24zcG03NjluZzhlZHA5OGNwdWdxYmQ4MmIiLCJjbGllbnRfc2VjcmV0IjoiMWU0c2dydGU4bGxxbGo0bmVjbWtzb292a2wzdjRmM2J2cXY3Y3A1bWQ5OW5pN2U3MGFtMSIsImV4cCI6MTY4NzQ1ODQ5MiwiaWF0IjoxNjg0ODY2NDkyLCJpc3MiOiJYaW9uIEdsb2JhbCBTZXJ2aWNlIEFQSSJ9.0cvfNKXFmK-OxmQiAMotrAFy0-zh8gio2AaNFXThzb8'
+                    let orderCode = event.data.orderCode;
+                    let orderStatus = 'pending';
+
+                    while (orderStatus !== 'successful') {
+                        const statusResponse = await axios.get(
+                            `https://devp-api.xion.app/api/v2/order/status/${orderCode}`,
+                            {
+                                headers: {
+                                    Authorization:
+                                        'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMHhFMTMxNzJhODI5RjBiQTYyMDIwQ0M4MDJlOGQ2OGFkNDM5NjNGOTgzIiwiY2xpZW50X2lkIjoiM24zcG03NjluZzhlZHA5OGNwdWdxYmQ4MmIiLCJjbGllbnRfc2VjcmV0IjoiMWU0c2dydGU4bGxxbGo0bmVjbWtzb292a2wzdjRmM2J2cXY3Y3A1bWQ5OW5pN2U3MGFtMSIsImV4cCI6MTY4NzQ1ODQ5MiwiaWF0IjoxNjg0ODY2NDkyLCJpc3MiOiJYaW9uIEdsb2JhbCBTZXJ2aWNlIEFQSSJ9.0cvfNKXFmK-OxmQiAMotrAFy0-zh8gio2AaNFXThzb8',
+                                },
+                            }
+                        );
+
+                        const statusData = statusResponse.data;
+                        console.log('Order status response:', statusData);
+
+                        orderStatus = statusData.status;
+                        console.log(`Order status: ${orderStatus}`);
+
+                        if (orderStatus !== 'successful') {
+                            // Add some delay to prevent overloading the API
+                            await new Promise(resolve => setTimeout(resolve, 5000));
                         }
-                    });
-
-                    const statusData = statusResponse.data;
-                    console.log('Order status response:', statusData);
-
-                    orderStatus = statusData.status;
-                    console.log(`Order status: ${orderStatus}`);
-
-                    if (orderStatus !== 'successful') {
-                        // Add some delay to prevent overloading the API
-                        await new Promise(resolve => setTimeout(resolve, 5000));
                     }
                 }
-            }
-        }, false);
-        
+            },
+            false
+        );
     } catch (error) {
         console.error('Error:', error);
+    }
+});
+
+// Display exchange rate as user enters ZAR amount
+document.getElementById('amount-input').addEventListener('input', async function() {
+    const zarAmount = parseFloat(this.value);
+    if (!isNaN(zarAmount)) {
+        const exchangeRateResponse = await axios.get('https://api.exchangerate-api.com/v4/latest/USD');
+        const exchangeRateData = exchangeRateResponse.data;
+        const usdRate = exchangeRateData.rates.ZAR;
+
+        const usdAmount = zarAmount / usdRate;
+        const exchangeRateText = `R${zarAmount.toFixed(2)} = $${usdAmount.toFixed(2)}`;
+        document.getElementById('exchange-rate').textContent = exchangeRateText;
+    } else {
+        document.getElementById('exchange-rate').textContent = '';
     }
 });
